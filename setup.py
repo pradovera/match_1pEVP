52bc8ad3910d2655261071117a1b08863f1a4302
Davide Pradovera <davidepradovera@gmail.com>
2025-11-13 10:07:37 +0100

    Updated version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_directory = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
 setup(name="match_1pEVP",
       description="Match-based 1-parameter eigensolver",
-      version="1.0.0",
+      version="1.1.0",
       license="GNU Library or Lesser General Public License (LGPL)",
       packages=find_packages(package_directory),
       zip_safe=False
